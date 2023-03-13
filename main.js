d913b4ed455debef41e0e84bc41244c8202c7491
Malin Ahlin <malinahlin1@gmail.com>
2023-03-13 14:50:45 +0100

    fixed toggle lightmode and wrong spelling

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import './style.css';
 import {createHtml} from './src/js/createHtml';
+import {toggleLightMode} from './src/js/toggleDarkmode'
 
-function init (){
+function init () {
 
-
-
-toggleLightMode();
+toggleLightMode("dark-mode");
     createHtml ();
 
 }
