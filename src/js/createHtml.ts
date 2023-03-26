ba5b74dc9145ea565d3793efde3b23e0628f1784
Malinaahlin <malinahlin1@gmail.com>
2023-03-26 23:37:54 +0200

    [refactor] - change deploy.yml frpm pnpm to npm

--- src/js/createHtml.ts
+++ src/js/createHtml.ts
@@ -5,7 +5,6 @@ const podCastContainer = document.querySelector('.section__podlist-pods') as HTM
 export async function createHtml() {
   const podCasts = await getPodcasts();
   for (let i = 0; i < podCasts.programs.length; i++) {
-
     const getAllPodcasts = podCasts.programs[i];
     const alt = [
       'Omslagsbild för Mammas nya kille podcast.',
@@ -20,7 +19,7 @@ export async function createHtml() {
     ];
 
     const innerArticle = document.createElement('article') as HTMLElement;
-    innerArticle.className='section__article-innerarticle';
+    innerArticle.className = 'section__article-innerarticle';
     podCastContainer.appendChild(innerArticle);
 
     const podcastImg = document.createElement('IMG') as HTMLImageElement;
@@ -29,7 +28,7 @@ export async function createHtml() {
     innerArticle.appendChild(podcastImg);
 
     const textDiv = document.createElement('div') as HTMLDivElement;
-    textDiv.className='section__article-div';
+    textDiv.className = 'section__article-div';
     innerArticle.appendChild(textDiv);
 
     const podcastTitle = document.createElement('h2') as HTMLElement;
@@ -47,7 +46,6 @@ export async function createHtml() {
 
     document.body.appendChild(podCastContainer);
   }
-
 }
 
 export default createHtml;
